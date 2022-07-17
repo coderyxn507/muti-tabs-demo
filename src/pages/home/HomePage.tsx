b5b88424e0895dbf8e07098049fef994949be9d1
yangxinni <53033515+coderyxn507@users.noreply.github.com>
2022-07-17 23:09:50 +0800

    xixi

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './HomePage.less';
 
-const HomePage = () => <div className={styles.root}>This is HomePage...</div>;
+const HomePage = () => <div className={styles.root}>This is New HomePage...</div>;
 
 export default HomePage;
